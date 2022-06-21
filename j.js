ec8d69ba7a374794bcb9c2115d75a5a1aca3a279
SkullGrandeur <81459492+SkullGrandeur@users.noreply.github.com>
2022-06-21 20:35:06 +0530

    Add files via upload

---
+++ j.js
@@ -0,0 +1,20 @@
+https://teachablemachine.withgoogle.com/models/2Tm10CRru/
+
+Webcam.set({
+    width : 350,
+    height : 300,
+    image_format : 'png',
+    png_quality : 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera');
+{
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id+"captured_image" src='+data_uri'"/>';
+
+})}
+
+console.log('ml5 version', ml5.version);
+classifier = ml5
